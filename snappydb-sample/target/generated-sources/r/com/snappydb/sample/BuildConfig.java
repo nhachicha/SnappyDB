02efec759a319b1c39ef8bddb2d5591cdbdc3415
Nabil HACHICHA <Nabil@Nabils-MacBook-Air.local>
2013-12-18 01:18:26 +0100

    remove generated target dir

--- snappydb-sample/target/generated-sources/r/com/snappydb/sample/BuildConfig.java
+++
@@ -1,5 +0,0 @@
-package com.snappydb.sample;
-
-public final class BuildConfig {
-  public static final boolean DEBUG = true;
-}
--- snappydb-tests/target/generated-sources/r/com/snappydb/sample/tests/BuildConfig.java
+++
@@ -1,5 +0,0 @@
-package com.snappydb.sample.tests;
-
-public final class BuildConfig {
-  public static final boolean DEBUG = true;
-}
